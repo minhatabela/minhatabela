05e28017f79b35ad0afc8cf00dfeb613258cbcb2
Alexis Lopes <alexisdrops@gmail.com>
2024-12-07 20:48:00 -0300

    adiciona lógicas para montagem da tabela

---
+++ pages/index.vue
@@ -0,0 +1,138 @@
+<script setup lang="ts">
+
+const { data } = await useAsyncData(
+  'jogos',
+  () => $fetch(
+    'https://api.gcn.ge.globo.com/api/simuladores/estado-campeonato/campeonato-brasileiro/campeonato-brasileiro-2024/', {
+    method: 'GET'
+  })
+)
+
+const { equipes, jogos, sedes } = data.value
+
+const tabela = computed(() => {
+  return useSortBy(Object.keys(equipes).map(Number).map(calculaStatsEquipe), ['pontos', 'vitorias', 'diferenca_gols', 'gols_pro']).toReversed()
+})
+
+function calculaStatsEquipe(equipe_id: number) {
+  const jogos_equipe = filtraJogosEquipe(equipe_id)
+
+  const gols_pro_mandante = jogos_equipe
+    .filter(jogo => jogo.equipe_mandante.id === equipe_id)
+    .map(jogo => jogo.placar_oficial_mandante)
+    .reduce((placar_atual, placar_prox) => placar_atual + placar_prox, 0)
+
+  const gols_contra_mandante = jogos_equipe
+    .filter(jogo => jogo.equipe_mandante.id === equipe_id)
+    .map(jogo => jogo.placar_oficial_visitante)
+    .reduce((placar_atual, placar_prox) => placar_atual + placar_prox, 0)
+
+  const gols_pro_visitante = jogos_equipe
+    .filter(jogo => jogo.equipe_visitante.id === equipe_id)
+    .map(jogo => jogo.placar_oficial_visitante)
+    .reduce((placar_atual, placar_prox) => placar_atual + placar_prox, 0)
+
+  const gols_contra_visitante = jogos_equipe
+    .filter(jogo => jogo.equipe_visitante.id === equipe_id)
+    .map(jogo => jogo.placar_oficial_mandante)
+    .reduce((placar_atual, placar_prox) => placar_atual + placar_prox, 0)
+
+  const vitorias = jogos_equipe
+    .filter(jogo => jogo.vencedor_jogo === equipe_id)
+
+  const empates = jogos_equipe
+    .filter(jogo => jogo.empate)
+
+  return {
+    gols_pro: gols_pro_mandante + gols_pro_visitante,
+    gols_contra: gols_contra_mandante + gols_contra_visitante,
+    vitorias: vitorias.length,
+    empates: empates.length,
+    derrotas: Math.abs((vitorias.length + empates.length) - jogos_equipe.length),
+    pontos: vitorias.length * 3 + empates.length,
+    diferenca_gols: (gols_pro_mandante + gols_pro_visitante) - (gols_contra_mandante + gols_contra_visitante),
+    equipe: equipes[equipe_id].nome_popular,
+    partidas: jogos_equipe.length,
+    clube_url: equipes[equipe_id].escudo.svg,
+  }
+
+}
+
+function filtraJogosEquipe(equipe_id: number) {
+  const contemEquipe = (jogo) => jogo.equipe_mandante.id === equipe_id || jogo.equipe_visitante.id === equipe_id
+  return Object.values(jogos || {}).filter(contemEquipe).filter(jogo => jogo.is_finalizado)
+}
+
+const columns: { key: string, label: string }[] = [
+  {
+    label: 'Pos',
+    key: 'posicao'
+  },
+  {
+    label: 'Clube',
+    key: 'equipe'
+  },
+  {
+    label: 'Pontos',
+    key: 'pontos'
+  },
+  {
+    label: 'Vitórias',
+    key: 'vitorias'
+  },
+  {
+    label: 'Empates',
+    key: 'empates'
+  },
+  {
+    label: 'Derrotas',
+    key: 'derrotas'
+  },
+  {
+    label: 'Partidas',
+    key: 'partidas'
+  },
+  {
+    label: 'Gols pró',
+    key: 'gols_pro'
+  },
+  {
+    label: 'Gols contra',
+    key: 'gols_contra'
+  },
+  {
+    label: 'Diff',
+    key: 'diferenca_gols'
+  }
+]
+
+function badgeColor(position: number) {
+  if (position >= 1 && position <= 6) return 'green'
+  if (position >= 7 && position <= 8) return 'orange'
+  if (position >= 9 && position <= 14) return 'blue'
+  if (position >= 15 && position <= 16) return 'white'
+  if (position >= 17 && position <= 20) return 'red'
+}
+
+</script>
+
+<template>
+  <div>
+    <UCard>
+      <UTable :columns="columns" :rows="tabela">
+        <template #posicao-data="{ row }">
+          <UBadge :color="badgeColor(tabela.indexOf(row) + 1)" :ui="{ rounded: 'rounded-full' }"
+            :label="tabela.indexOf(row) + 1" />
+        </template>
+        <template #equipe-data="{ row }">
+          <div style="display: flex; gap: 8px; ">
+            <img style="width: 20px;" :src="row.clube_url" alt="">
+            {{ row.equipe }}
+          </div>
+        </template>
+      </UTable>
+    </UCard>
+  </div>
+</template>
+
+<style scoped></style>

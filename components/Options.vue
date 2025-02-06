f4ff55c9513056417b40037c430ad77d17ff133f
Alexis Lopes <alexisdrops@gmail.com>
2025-02-05 22:26:13 -0300

    ajusta escala das opções do atalho de resultado

--- components/Options.vue
+++ components/Options.vue
@@ -7,23 +7,23 @@
         <div class="flex flex-col">
           <span @click="atribuirVitoriaSimplesMandante"
             class="px-4 flex gap-2 items-center py-2 hover:bg-slate-700 cursor-pointer">
-            <img class="h-6 w-6" :src="partida.mandante.escudo" alt="">
+            <img class="h-4 w-4" :src="partida.mandante.escudo" alt="">
             {{
               partida.mandante.nome_popular }} vence</span>
           <span @click="atribuirVitoriaSimplesVisitante"
             class="px-4 flex gap-2 items-center py-2 hover:bg-slate-700 cursor-pointer">
-            <img class="h-6 w-6" :src="partida.visitante.escudo" alt="">
+            <img class="h-4 w-4" :src="partida.visitante.escudo" alt="">
             {{
               partida.visitante.nome_popular }} vence</span>
           <span @click="decretarEmpateSimples"
             class="px-4 flex gap-2 items-center py-2 hover:bg-slate-700 cursor-pointer">
-            <Icon size="1.5rem" name="simple-line-icons:minus" />
+            <Icon size="1rem" name="simple-line-icons:minus" />
             Empate
           </span>
           <UDivider v-if="partida.status != 'finalizada' && simulacao.get(partida.id)" class="py-1" />
           <span @click="confirm = true" v-if="partida.status != 'finalizada' && !confirm && simulacao.get(partida.id)"
             class="px-4 flex gap-2 items-center py-2 hover:bg-slate-700 cursor-pointer text-red-500">
-            <Icon size="1.5rem" name="mynaui:trash" />
+            <Icon size="1rem" name="mynaui:trash" />
 
             Limpar
             simulação

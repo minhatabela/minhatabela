e21a03650f6f4c9a338178b995846521b5ae5726
Alexis Lopes <alexisdrops@gmail.com>
2025-02-10 21:34:32 -0300

    limpa simulação local ao fazer logout

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,7 +1,11 @@
 const toast = useToast()
 const supabase = useSupabaseClient()
 
+
+
 export const useAuth = () => {
+
+  const { simulacao } = useSimulador()
 
   async function login() {
     const { error } = await supabase.auth.signInWithOAuth({ provider: 'google' })
@@ -15,6 +19,8 @@ export const useAuth = () => {
     const { error } = await supabase.auth.signOut()
     if (error) {
       toast.add({ title: 'Erro ao tentar sair', color: 'red' })
+    } else {
+      simulacao.value.clear()
     }
   }
 

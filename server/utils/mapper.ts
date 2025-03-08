09ef4372a2c9e410539638658c16574c2cd4073d
Alexis Lopes <alexisdrops@gmail.com>
2025-03-07 22:42:01 -0300

    chore: separa lógica do endpoint de consistência

---
+++ server/utils/mapper.ts
@@ -0,0 +1,29 @@
+import { Jogo, PartidaCBF, PartidaNormalizada } from '~/types/jogo'
+
+export function mapPartidaCBF(partida: PartidaCBF): PartidaNormalizada {
+  return {
+    numero: Number(partida.num_jogo),
+    rodada: Number(partida.rodada),
+    mandante: partida.mandante.nome,
+    visitante: partida.visitante.nome,
+    gols_mandante: partida.mandante.gols || undefined,
+    gols_visitante: partida.visitante.gols || undefined,
+    sede: partida.local === " -  - " ? undefined : partida.local,
+    data: partida.data === "A Definir" ? undefined : partida.data,
+    hora: partida.hora || undefined
+  }
+}
+
+export function mapPartidaMT(partida: Jogo): PartidaNormalizada {
+  return {
+    numero: partida.numero || undefined,
+    rodada: partida.rodada,
+    data: partida.data || undefined,
+    hora: partida.hora || undefined,
+    gols_mandante: partida.gols_mandante || undefined,
+    gols_visitante: partida.gols_visitante || undefined,
+    visitante: partida.visitante.slug,
+    mandante: partida.mandante.slug,
+    sede: partida.sede?.nome_popular || undefined
+  }
+}
--- types/jogo.ts
+++ types/jogo.ts
@@ -1,2 +1,29 @@
 import { type Tables } from "~/types/database.types";
 export type Jogo = Omit<Tables<'partida'>, 'visitante' | 'mandante'> & { mandante: Tables<'clube'>, visitante: Tables<'clube'> }
+
+export interface ClubeCBF {
+  nome: string,
+  gols?: number,
+}
+
+export interface PartidaCBF {
+  num_jogo: string,
+  rodada: string,
+  mandante: ClubeCBF,
+  visitante: ClubeCBF,
+  local: string,
+  data: string,
+  hora: string
+}
+
+export interface PartidaNormalizada {
+  numero?: number
+  rodada: number
+  mandante: string
+  visitante: string
+  gols_mandante?: number
+  gols_visitante?: number
+  sede?: string
+  data?: string
+  hora?: string
+}

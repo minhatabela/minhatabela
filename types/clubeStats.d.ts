b96ef55f91681449591bef8dc05ae20cd0368c11
Alexis Lopes <alexisdrops@gmail.com>
2025-03-28 21:56:28 -0300

    misc: adiciona vitests as dependencias e tipo de clubestats

---
+++ types/clubeStats.d.ts
@@ -0,0 +1,12 @@
+export type IClubeStats = {
+  vitorias: number
+  empates: number
+  derrotas: number
+  gols_pro: number
+  gols_contra: number
+  escudo: string
+  partidas: number
+  pontos: number
+  golsDif: number
+  clube: string
+}

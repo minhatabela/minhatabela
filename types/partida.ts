baaff28c99413a9604fb7567fcccb3a5c8854e13
Alexis Lopes <alexisdrops@gmail.com>
2025-03-31 21:45:53 -0300

    chore: cria tipos para clube, partida e sede

--- types/partida.ts
+++ types/partida.ts
@@ -1,5 +1,7 @@
 import { type Tables } from "~/types/database.types";
-export type Partida = Omit<Tables<'partida'>, 'visitante' | 'mandante'> & { mandante: Tables<'clube'>, visitante: Tables<'clube'>, sede: Tables<'sede'> }
+import { type IClube } from "./clube";
+import { type ISede } from "./sede";
+export type Partida = Omit<Tables<'partida'>, 'visitante' | 'mandante' | 'sede' | 'created_at' | 'updated_at'> & { mandante: IClube, visitante: IClube, sede: ISede }
 
 export interface IPartida extends Partida { }
 

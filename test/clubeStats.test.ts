dbc08d0f51216fc13e940fe3133642f6cf15c601
Alexis Lopes <alexisdrops@gmail.com>
2025-03-31 21:51:53 -0300

    test: refatora testes de clubestats para gerar dados de classes

--- test/clubeStats.test.ts
+++ test/clubeStats.test.ts
@@ -1,15 +1,59 @@
 import { describe, expect, test } from 'vitest'
-import type { Clube } from '~/types/clube'
-import type { Partida } from '~/types/partida'
+import { CampeonatoGenerator } from '~/models/CampeonatoGenerator'
 import { ClubeStats } from '~/utils/clubeStats'
-import clubes from './mocks/clubes.json'
-import partidas from './mocks/partidas.json'
 
 describe('ClubeStats', () => {
 
   function setup() {
-    const clube = clubes.find(clube => clube.sigla === 'COR') as Clube
-    const partidasCampeonato = partidas as Partida[]
+
+    const results = [
+      [2, 1],
+      [1, 0],
+      [0, 0],
+      [3, 2],
+      [1, 1],
+      [0, 2],
+      [2, 0],
+      [1, 3],
+      [0, 0],
+      [2, 2],
+
+      [1, 0],
+      [0, 1],
+      [3, 1],
+      [2, 3],
+      [1, 1],
+      [0, 0],
+      [2, 1],
+      [1, 2],
+      [0, 3],
+      [3, 0],
+      [1, 1],
+      [2, 0],
+      [0, 1],
+      [3, 2],
+      [1, 3],
+      [0, 0],
+      [2, 2],
+      [1, 0],
+      [0, 2],
+      [3, 1],
+
+      [1, 1],
+      [2, 3],
+      [0, 0],
+      [1, 2],
+      [3, 0],
+      [2, 1],
+      [0, 1],
+      [1, 0]
+    ]
+
+    const campeonatoGenerator = new CampeonatoGenerator()
+    const clubesCampeonato = campeonatoGenerator.gererarClubes()
+    const partidasCampeonato = campeonatoGenerator.gerarPartidas({ clube: 'Corinthians', results })
+    const clube = clubesCampeonato[0]
+
     const clubeStats = new ClubeStats(clube, partidasCampeonato)
     return { clube, clubeStats }
   }
@@ -17,71 +61,71 @@ describe('ClubeStats', () => {
 
   test('should filter partidas by clube', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.partidas.length).toBe(5)
+    expect(clubeStats.partidas.length).toBe(38)
   })
 
   test('should count victories', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.vitorias().length).toBe(2)
+    expect(clubeStats.vitorias().length).toBe(15)
   })
 
   test('should count defeats', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.derrotas().length).toBe(2)
+    expect(clubeStats.derrotas().length).toBe(12)
   })
 
   test('should count draws', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.empates().length).toBe(1)
+    expect(clubeStats.empates().length).toBe(11)
   })
 
   test('should count points', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.pontos()).toBe(7)
+    expect(clubeStats.pontos()).toBe(56)
   })
 
   test('should count goals pro', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.golsPro()).toBe(4)
+    expect(clubeStats.golsPro()).toBe(48)
   })
 
   test('should count goals against', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.golsContra()).toBe(4)
+    expect(clubeStats.golsContra()).toBe(43)
   })
 
   test('should filter home games', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.partidasMandante().length).toBe(3)
+    expect(clubeStats.partidasMandante().length).toBe(19)
   })
 
   test('should filter away games', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.partidasVisitante().length).toBe(2)
+    expect(clubeStats.partidasVisitante().length).toBe(19)
   })
 
   test('should count goals difference', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.golsDif()).toBe(0)
+    expect(clubeStats.golsDif()).toBe(5)
   })
 
   test('should count matches with results', () => {
     const { clubeStats } = setup()
-    expect(clubeStats.partidasComResultado().length).toBe(5)
+    expect(clubeStats.partidasComResultado().length).toBe(38)
   })
 
   test('should return stats object', () => {
     const { clubeStats, clube } = setup()
     expect(clubeStats.stats()).toEqual({
-      gols_pro: 4,
-      gols_contra: 4,
-      vitorias: 2,
-      empates: 1,
-      derrotas: 2,
-      pontos: 7,
-      golsDif: 0,
+      gols_pro: 48,
+      gols_contra: 43,
+      vitorias: 15,
+      empates: 11,
+      derrotas: 12,
+      pontos: 56,
+      golsDif: 5,
       clube: clube?.nome_popular || '',
-      partidas: 5,
+      partidas: 38,
       escudo: clube?.escudo || ''
     })
   })

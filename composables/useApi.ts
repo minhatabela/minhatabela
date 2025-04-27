12d4b2af4651f4b2704948f902f53cdce5e5c94b
Alexis Lopes <alexisdrops@gmail.com>
2025-04-27 20:47:02 -0300

    chore: adiciona refresh de sedes ao retorno da composable de useApi

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -4,11 +4,11 @@ import type { ISede } from "~/types/sede"
 
 const { data: partidas } = useAsyncData('partidas', () => $fetch<IPartida[]>('/api/partidas'), { default: () => [] })
 const { data: clubes } = useAsyncData('clubes', () => $fetch<IClube[]>('/api/clubes'), { default: () => [] })
-const { data: sedes } = useAsyncData('sedes', () => $fetch<ISede[]>('/api/sedes'), { default: () => [] })
+const { data: sedes, refresh: refreshSedes } = useAsyncData('sedes', () => $fetch<ISede[]>('/api/sedes'), { default: () => [] })
 
 
 
 
 export const useApi = () => {
-  return { partidas, clubes, sedes }
+  return { partidas, clubes, sedes, refreshSedes }
 }

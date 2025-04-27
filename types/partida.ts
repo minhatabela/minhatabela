033ab4fd018c58675223dc1a5e9cb9b007c044d8
Alexis Lopes <alexisdrops@gmail.com>
2025-04-27 20:47:54 -0300

    type: adiciona tipo para partidas inconsistentes

--- types/partida.ts
+++ types/partida.ts
@@ -5,6 +5,18 @@ export type Partida = Omit<Tables<'partida'>, 'visitante' | 'mandante' | 'sede' 
 
 export interface IPartida extends Partida { }
 
+export interface CamposInconsistentes {
+  gols_mandante?: number
+  gols_visitante?: number
+  sede?: string
+  data?: string
+  hora?: string
+}
+
+export interface PartidaConsistencia extends IPartida {
+  inconsistencias: CamposInconsistentes
+}
+
 export interface ClubeCBF {
   nome: string,
   gols?: number,

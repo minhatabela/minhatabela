291d6307e67d42af02e6eeb2ad9dabc2e9a2c532
Alexis Lopes <alexisdrops@gmail.com>
2025-06-15 20:10:50 -0300

    🧹 chore(app): remove useTabela composable

--- layers/standings/application/components/Standings.view.vue
+++ layers/standings/application/components/Standings.view.vue
@@ -4,9 +4,8 @@ import { TableViewEnum } from '../../domain/enums/TableView.enum'
 import { StandignsHeaderFactory } from '../../domain/factories/StandingsHeader.factory'
 import { MatchMap } from '../../infra/mappers/Match.map'
 import { TeamMap } from '../../infra/mappers/Team.map'
-import { useTabela } from '../composables/useTabela'
 
-const { tableView } = useTabela()
+const tableView = ref(TableViewEnum.OFICIAL_SIMULADA)
 
 const tableViewOptions = [
   { label: 'Oficial Simulada', value: TableViewEnum.OFICIAL_SIMULADA },

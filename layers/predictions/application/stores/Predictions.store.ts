494e8ee845c5ea6e7f47232426b54a1aab11329c
Alexis Lopes <alexisdrops@gmail.com>
2025-06-19 20:16:06 -0300

    feat: add predictions store

---
+++ layers/predictions/application/stores/Predictions.store.ts
@@ -0,0 +1,14 @@
+import type { Match } from "~~/layers/shared/entities/Match"
+
+export const usePredictionsStore =  defineStore('predictions', () => {
+  const predictions = ref<Match[]>()
+
+  function setPredictions(_predictions: Match[]) {
+    predictions.value = _predictions
+  }
+
+  return {
+    predictions,
+    setPredictions
+  }
+})

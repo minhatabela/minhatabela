993add1b51881320c10cf27c4a33b83e241f9159
Alexis Lopes <alexisdrops@gmail.com>
2025-06-19 19:31:35 -0300

    feat: create syncing component

--- app/pages/simular.vue
+++ app/pages/simular.vue
@@ -7,44 +7,16 @@ useHead({
   title: 'Simulando'
 })
 
-const { componentToPng } = useHtmlToImage()
 const { jogosRodada, rodada_navegavel, syncing, simulacao, execute } = useSimulador()
 
-const arte = ref()
-
-const empty = computed(() => {
-  const jogosRodadaIds = jogosRodada.value.map(m => m.id)
-  const idsSimulacao = Array.from(simulacao.value?.keys()) || []
-
-  return !jogosRodadaIds.some(partidaId => idsSimulacao.includes(partidaId))
-})
 </script>
 
 <template>
-  <div v-show="false">
-    <ArteRodada
-      :rodada="rodada_navegavel"
-      ref="arte"
-    />
-  </div>
   <div class="flex flex-col xl:flex-row gap-16 lg:px-0 px-8 justify-between">
     <StandingsView />
     <div class="flex w-full flex-col gap-4">
       <div class="flex justify-between items-center">
-        <UIcon
-          name="i-ion-sync"
-          class="w-4 h-4 text-green-400"
-          :class="{ 'animate-spin  text-black dark:text-white': syncing }"
-        />
-        <UButton
-          :disabled="empty"
-          @click="componentToPng(arte, rodada_navegavel)"
-          size="xs"
-          variant="ghost"
-          color="primary"
-          icon="i-ic-round-download"
-          label="baixar simulação da rodada"
-        />
+        <Syncing :syncing="syncing" />
       </div>
       <RoundPagination v-model="rodada_navegavel" />
       <div

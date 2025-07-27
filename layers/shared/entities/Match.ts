c085da591c37055cbb7030dc5424585407a79796
Alexis Lopes <alexisdrops@gmail.com>
2025-07-27 19:55:37 -0300

    chore: change postponed logic and add realizationDateTime getter to Match

--- layers/shared/entities/Match.ts
+++ layers/shared/entities/Match.ts
@@ -1,10 +1,11 @@
-import { isThisWeek } from 'date-fns'
+import { format, isThisWeek } from 'date-fns'
 import type { MatchDate } from '../values/MatchDate'
 import type { MatchNumber } from '../values/MatchNumber'
 import type { MatchTime } from '../values/MatchTime'
 import type { Round } from '../values/Round'
 import type { Team } from './Team'
 import type { Vanue } from './Vanue'
+import { ptBR } from 'date-fns/locale'
 
 export class Match {
   constructor(
@@ -25,11 +26,19 @@ export class Match {
   }
 
   get isPostponed(): boolean {
-    return !isDefined(this.date.value) && !isDefined(this.time.value)
+    return !isDefined(this.date.value) || !isDefined(this.time.value)
   }
 
   get isThisWeek(): boolean {
     if (!this.date.dateValue) return false
     return isThisWeek(this.date.dateValue)
+  }
+
+  get realizationDateTime(): string | undefined {
+    if (!this.date.dateValue || !this.time.value) return undefined
+
+    const [hours, mins, secs] = this.time.value.split(':').map(Number)
+
+    return format(this.date.dateValue.setHours(hours!, mins, secs), 'd MMM HH:mm', { locale: ptBR })
   }
 }

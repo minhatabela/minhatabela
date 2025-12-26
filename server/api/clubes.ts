7cb7cdf8fcad2513591aa4ae37b845c423c3410e
Alexis Lopes <alexisdrops@gmail.com>
2025-12-26 20:26:01 -0300

    chore: filter teams not archived (#47)

--- server/api/clubes.ts
+++ server/api/clubes.ts
@@ -3,7 +3,7 @@ import { serverSupabaseServiceRole } from '#supabase/server'
 export default defineEventHandler(async event => {
   const client = serverSupabaseServiceRole(event)
 
-  const { data } = await client.from('clube').select()
+  const { data } = await client.from('clube').select().eq('archived', false)
 
   return data
 })

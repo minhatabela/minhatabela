5fb38d24bfa8f618e0bde83a631cd842bd507620
Alexis Lopes <alexisdrops@gmail.com>
2025-12-26 19:47:25 -0300

    chore: update nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,12 +45,6 @@ export default defineNuxtConfig({
   },
   pages: true,
   ssr: true,
-  routeRules: {
-    '/simular': { ssr: false },
-    '/': { ssr: true },
-    '/admin/consistencia': { ssr: false },
-    '/admin/partidas': { ssr: false }
-  },
   app: {
     head: {
       link: [
